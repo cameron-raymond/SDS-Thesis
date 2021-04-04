b28188f6b4d61d00727ed472fe5b95bec520772d
Cameron Raymond <cameron.raymond@hey.com>
2021-04-04 20:33:47 +0100

    this is going to be a lot of commits

--- functions/participants/participants.js
+++ functions/participants/participants.js
@@ -32,29 +32,29 @@ if (!admin.apps.length) {
 const firestore = admin.firestore()
 
 exports.handler = async (event, context) => {
-  let data = JSON.parse(event.body)
-  const { PROLIFIC_PID } = data
-  // const { email, subscriptionId } = event.queryStringParameters
-  console.log("Creating a new participant for " + PROLIFIC_PID)
-  try {
-    const query = await firestore.collection('participants').doc(PROLIFIC_PID.toString()).set(data)
-    console.log(query)
-    if (query.empty) {
-      return {
-        statusCode: 404,
-        body: JSON.stringify({
-          message: 'User not found'
-        })
-      }
-    }
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        error: error.message
-      })
-    }
-  }
+  // let data = JSON.parse(event.body)
+  // const { PROLIFIC_PID } = data
+  // // const { email, subscriptionId } = event.queryStringParameters
+  // console.log("Creating a new participant for " + PROLIFIC_PID)
+  // try {
+  //   const query = await firestore.collection('participants').doc(PROLIFIC_PID.toString()).set(data)
+  //   console.log(query)
+  //   if (query.empty) {
+  //     return {
+  //       statusCode: 404,
+  //       body: JSON.stringify({
+  //         message: 'User not found'
+  //       })
+  //     }
+  //   }
+  // } catch (error) {
+  //   return {
+  //     statusCode: 500,
+  //     body: JSON.stringify({
+  //       error: error.message
+  //     })
+  //   }
+  // }
   return {
     statusCode: 200,
     /* Required for CORS support to work */
