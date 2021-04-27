fd3679bc7a5d1567800e1356757e7326add1149e
Cameron Raymond <cameron.raymond@hey.com>
2021-04-27 10:35:24 +0100

    branch set up!

---
+++ src/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+import sirv from 'sirv';
+import polka from 'polka';
+import compression from 'compression';
+import * as sapper from '@sapper/server';
+
+// const express = require('express')
+const app = polka()
+const bodyParser = require('body-parser')
+
+const { PORT, NODE_ENV } = process.env;
+const dev = NODE_ENV === 'development';
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(
+	compression({ threshold: 0 }),
+	sirv('static', { dev }),
+	sapper.middleware()
+)
+	.listen(PORT, err => {
+		if (err) console.log('error', err);
+	});
