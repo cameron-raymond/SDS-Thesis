2fe6ebc75ff0821490912bd41237194518b5feff
Cameron Raymond <cameron.raymond@hey.com>
2021-05-03 12:06:20 +0100

    new video player w/ captions

--- src/components/Video.svelte
+++ src/components/Video.svelte
@@ -1,6 +1,18 @@
 <script>
-  import VideoPlayer from "svelte-video-player";
-  export let source = ["/rumour_context.webm", "/rumour_context.mp4"];
+  import { Player, Video, DefaultUi } from "@vime/svelte";
 </script>
 
-<VideoPlayer {source} />
+<Player theme="dark" loop=false >
+  <Video crossOrigin>
+    <source src='/rumour_context.webm' type='video/webm' />
+    <source src='/rumour_context.mp4' type='video/mp4' /> 
+
+    <track
+      default
+      kind="subtitles"
+      src="/rumour_context.vtt"
+      srclang="en"
+      label="English" />
+  </Video>
+  <DefaultUi />
+</Player>
