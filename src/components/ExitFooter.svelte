f6570eca633148313b0c559c2d8e3e93846ba5ea
Cameron Raymond <cameron.raymond@hey.com>
2021-06-15 08:55:15 +0100

    doesn't interact with DB anymore

--- src/components/ExitFooter.svelte
+++ src/components/ExitFooter.svelte
@@ -7,28 +7,6 @@
   const finish = async () => {
     if (confirm(message)) {
       consent.set(0);
-      const deletePackage = {
-        method: "DELETE",
-        body: JSON.stringify({ PROLIFIC_PID: $PROLIFIC_PID }),
-        headers: {
-          "Content-Type": "application/json"
-        }
-      };
-      const collections = [
-        "participants",
-        "rumour-one-check",
-        "rumour-one-results",
-        "rumour-two-check",
-        "rumour-two-results",
-        "post-study-responses"
-      ];
-      for (const collection of collections) {
-        const url = "/.netlify/functions/azure?collection=" + collection;
-        fetch(url, deletePackage).catch(err => {
-          console.log(url);
-          console.log("DELETE error", err.message);
-        });
-      }
     }
   };
 </script>
